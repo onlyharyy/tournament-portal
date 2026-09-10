7223222de0576b293d590fda5014d7f20bb1e8fe
onlyharyy <PRAJAPATIHARRY198@GMAIL.COM>
2026-09-10 13:00:53 +0530

    Add admin setup

--- build.sh
+++ build.sh
@@ -4,4 +4,14 @@ pip install -r requirements.txt
 
 python manage.py migrate
 
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+username = os.getenv('ADMIN')
+password = os.getenv('123456')
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, password=password)
+"
+
 python manage.py collectstatic --noinput
