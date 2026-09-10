6eee2857d7f6bd1e75542186e6e7e169a5becc7b
onlyharyy <PRAJAPATIHARRY198@GMAIL.COM>
2026-09-10 13:13:14 +0530

    Fix admin creation

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ python manage.py shell -c "
 from django.contrib.auth import get_user_model
 import os
 User = get_user_model()
-username = os.getenv('ADMIN')
-password = os.getenv('123456')
+username = os.getenv('ADMIN_USERNAME')
+password = os.getenv('ADMIN_PASSWORD')
 if username and password and not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, password=password)
 "
